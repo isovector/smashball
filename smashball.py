1432956a0c6998aed8b90ef5deb4dfd375bc647e
Sandy Maguire <sandy.g.maguire@gmail.com>
2012-12-13 22:00:39 -0500

    SceneGraphs, hitboxes, cleaner code
    Added a class to manage all of the entities in the world
    Added hitboxes to exert forces on entities
    Lots of code refactoring

--- smashball.py
+++ smashball.py
@@ -1,12 +1,3 @@
-"""Showcase of a very basic 2d platformer
-
-The red girl sprite is taken from Sithjester's RMXP Resources:
-http://untamed.wild-refuge.net/rmxpresources.php?characters
-
-.. note:: The code of this example is a bit messy. If you adapt this to your 
-    own code you might want to structure it a bit differently.
-"""
-
 __version__ = "$Id:$"
 __docformat__ = "reStructuredText"
 
@@ -30,6 +21,60 @@ def cpflerpconst(f1, f2, d):
     return f1 + cpfclamp(f2 - f1, -d, d)
 
 
+class Entity(pymunk.Body):
+    def __init__(self, mass):
+        pymunk.Body.__init__(self, mass, pymunk.inf)
+        self.damage = 0
+        
+    def onRegister(self, space):
+        return
+        
+class Ball(Entity):
+    def __init__(self):
+        Entity.__init__(self, 5)
+        self.shape = pymunk.Circle(self, 12, (0, 0))
+        self.shape.color = pygame.color.THECOLORS['green']
+        self.shape.elasticity = 1
+        
+    def onRegister(self, space):
+        space.add(self.shape)
+        
+class Actor(Entity):
+    def __init__(self):
+        Entity.__init__(self, 5)
+        self.head = pymunk.Circle(self, 10, (0,5))
+        self.head2 = pymunk.Circle(self, 10, (0,13))
+        self.head2.elasticity = 0.95
+        self.feet = pymunk.Circle(self, 10, (0,-5))
+
+        self.head.layers = self.head2.layers = 0b1000
+        self.feet.collision_type = 1
+    
+    def onRegister(self, space):
+        space.add(self.head, self.head2, self.feet)
+    
+
+class SceneGraph:
+    def __init__(self, screen, space):
+        self.space = space
+        self.screen = screen
+        self.entities = []
+    
+    def register(self, object):
+        self.entities.append(object)
+        self.space.add(object)
+        object.onRegister(self.space)
+    
+    def hitbox(self, pos, radius, vec, source):
+        pygame.draw.circle(self.screen, pygame.color.THECOLORS['pink'], to_pygame(pos, self.screen), radius)
+        for entity in self.entities:
+            if entity != source:
+                if pos.get_distance(entity.position) < radius:
+                    entity.damage += 10
+                    entity.velocity = vec * entity.damage / 100
+    
+
+
 
 width, height = 690,400
 fps = 60
@@ -42,7 +87,7 @@ PLAYER_AIR_ACCEL_TIME = 0.25
 PLAYER_AIR_ACCEL = (PLAYER_VELOCITY/PLAYER_AIR_ACCEL_TIME)
 
 JUMP_HEIGHT = 26.*3
-JUMP_BOOST_HEIGHT = 28.
+JUMP_BOOST_HEIGHT = 36.
 JUMP_CUTOFF_VELOCITY = 100
 FALL_VELOCITY = 250.
 
@@ -51,7 +96,6 @@ HEAD_FRICTION = 0.7
 PLATFORM_SPEED = 1
 
 def main():
-
     ### PyGame init
     pygame.init()
     screen = pygame.display.set_mode((width,height)) 
@@ -59,9 +103,7 @@ def main():
     clock = pygame.time.Clock()
     running = True
     font = pygame.font.SysFont("Arial", 16)
-    #sound = pygame.mixer.Sound("sfx.wav")
-    #img = pygame.image.load("xmasgirl1.png")
-    
+
     ### Physics stuff
     space = pymunk.Space()   
     space.gravity = 0,-1000
@@ -74,37 +116,29 @@ def main():
                 , pymunk.Segment(space.static_body, (10, 370), (10, 50), 5)
                 ]  
     static[1].color = pygame.color.THECOLORS['red']
-    #static[2].color = pygame.color.THECOLORS['green']
-    #static[3].color = pygame.color.THECOLORS['red']
-    
 
     
     for s in static:
         s.friction = 1.
         s.group = 1
+        s.elasticity = 0.9
+        
     space.add(static)
+    space.elasticIterations = 10
 
+
+    scene = SceneGraph(screen, space)
    
-    # player
-    body = pymunk.Body(5, pymunk.inf)
+    body = Actor()
     body.position = 100,100
     
-    # ball
-    ballbody = pymunk.Body(5, pymunk.inf)
-    ballbody.position = 200, 200
-    ball = pymunk.Circle(ballbody, 12, (200, 100))
-    ball.color = pygame.color.THECOLORS['green']
-    ball.friction = 0.12
-    
-    
-    head = pymunk.Circle(body, 10, (0,5))
-    head2 = pymunk.Circle(body, 10, (0,13))
-    feet = pymunk.Circle(body, 10, (0,-5))
+    ball = Ball()
+    ball.position = 200,200
 
-    head.layers = head2.layers = 0b1000
-    feet.collision_type = 1
+    scene.register(ball)
+    scene.register(body)
     
-    space.add(body, head, feet,head2, ballbody, ball)
+
     direction = 1
     remaining_jumps = 2
     landing = {'p':Vec2d.zero(), 'n':0}
@@ -113,6 +147,8 @@ def main():
     landed_previous = False
     
     while running:
+        screen.fill(pygame.color.THECOLORS["black"])
+        
         
         grounding = {
             'normal' : Vec2d.zero(),
@@ -135,7 +171,7 @@ def main():
         body.each_arbiter(f)
             
         well_grounded = False
-        if grounding['body'] != None and abs(grounding['normal'].x/grounding['normal'].y) < feet.friction:
+        if grounding['body'] != None and abs(grounding['normal'].x/grounding['normal'].y) < body.feet.friction:
             well_grounded = True
             remaining_jumps = 2
     
@@ -147,6 +183,13 @@ def main():
             if event.type == QUIT or \
                 event.type == KEYDOWN and (event.key in [K_ESCAPE, K_q]):  
                 running = False
+            
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                ball.apply_impulse((ball.position - body.position + Vec2d(0, 100)).normalized() * 1000)
+            
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                scene.hitbox(body.position + Vec2d(20, 0), 20, Vec2d(0,400), body)
+                
             elif event.type == KEYDOWN and event.key == K_p:
                 pygame.image.save(screen, "platformer.png")
 
@@ -176,14 +219,14 @@ def main():
         if (keys[K_DOWN]):
             direction = -3
             
-        feet.surface_velocity = target_vx,0
+        body.feet.surface_velocity = target_vx,0
 
         
         if grounding['body'] != None:
-            feet.friction = -PLAYER_GROUND_ACCEL/space.gravity.y
-            head.friciton = HEAD_FRICTION
+            body.feet.friction = -PLAYER_GROUND_ACCEL/space.gravity.y
+            body.head.friciton = HEAD_FRICTION
         else:
-            feet.friction,head.friction = 0,0
+            body.feet.friction, body.head.friction = 0,0
         
         # Air control
         if grounding['body'] == None:
@@ -191,9 +234,7 @@ def main():
         
         body.velocity.y = max(body.velocity.y, -FALL_VELOCITY) # clamp upwards as well?
         
-        ### Clear screen
-        screen.fill(pygame.color.THECOLORS["black"])
-        
+       
         ### Helper lines
         for y in [50,100,150,200,250,300]:
             color = pygame.color.THECOLORS['darkgrey']
