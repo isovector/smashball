776fc5fb365298e1dc6f8b83feb9f758164673ce
Sandy Maguire <sandy.g.maguire@gmail.com>
2012-12-15 03:21:46 -0500

    First pass of an attack designer

---
+++ util/parse.py
@@ -0,0 +1,71 @@
+from math import sqrt
+from xml.dom.minidom import parse, parseString
+
+def getPoints(path):
+    points = []
+    pathString = path.getAttribute("d")[2:]
+    for point in pathString.split(" "):
+        x, y = point.split(",")
+        points.append((float(x), float(y)))
+    return points
+    
+def getTimeStamps(texts):
+    timestamps = []
+    for text in texts:
+        x = float(text.getAttribute("x"))
+        y = float(text.getAttribute("y"))
+        data = text.getElementsByTagName("tspan")[0].firstChild.data
+        timestamps.append(((x, y), data))
+    return timestamps
+    
+def getHitbox(hb):
+    x = float(hb.getAttribute("sodipodi:cx"))
+    y = float(hb.getAttribute("sodipodi:cy"))
+    r = float(hb.getAttribute("sodipodi:radius"))
+    return (x, y, r)
+    
+def associate(points, timestamps):
+    results = []
+    for i in range(len(points)):
+        best = None
+        bestdist = 9999999
+        for j in range(len(timestamps)):
+            dist = distance(points[i], timestamps[j][0])
+            if dist < bestdist:
+                best = timestamps[j][1]
+                bestdist = dist
+        results.append((points[i], best))
+    return results
+
+def distance(a, b):
+    x = a[0] - b[0]
+    y = a[1] - b[1]
+    return sqrt(x * x + y * y)
+
+dom = parse("./move.svg")
+dom = dom.getElementsByTagName("svg")[0]
+for layer in dom.getElementsByTagName("g"):
+    type = layer.getAttribute("inkscape:label")
+    if type == "Movement":
+        points = getPoints(layer.getElementsByTagName("path")[0])
+        timestamps  = getTimeStamps(layer.getElementsByTagName("text"))
+        print(associate(points, timestamps))
+        
+    elif type == "Damage":
+        hitboxes = []
+        knockbacks = []
+        for path in layer.getElementsByTagName("path"):
+            spiral = path.getAttribute("sodipodi:type")
+            if spiral:
+                hitboxes.append(getHitbox(path))
+            else:
+                knockbacks.append(getPoints(path))
+        knockbacks = [ ((x[0][0], x[0][1]), distance(x[0], x[1])) for x in knockbacks ]
+        timestamps  = getTimeStamps(layer.getElementsByTagName("text"))
+
+        hitKnocks = associate(hitboxes, knockbacks)
+        hitTimes = associate(hitboxes, timestamps)
+        
+        damage = [ ((x[0][0], x[0][1]), (x[1], y[1])) for x,y in zip(hitKnocks, hitTimes) ]
+        
+        print(damage)
